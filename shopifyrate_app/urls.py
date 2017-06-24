e3c312236a656e587f98fa48b6025c8827a53090
sergo <sergey@kilometer.io>
2017-06-24 18:23:31 +0300

    fixed

--- shopifyrate_app/urls.py
+++ shopifyrate_app/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, include
-from shopify_auth import urls as shopify_auth_urls
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index_page'),
-    url(r'^login/', include(shopify_auth_urls)),
+    url(r'^login/', include('shopify_auth.urls')),
 ]
