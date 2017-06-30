c6dd07fbfff251d3cdbbf5912c45d58f41317612
sergo <sergey@kilometer.io>
2017-06-30 23:24:21 +0300

    added react

--- shopifyrate_app/views.py
+++ shopifyrate_app/views.py
@@ -9,19 +9,16 @@ from shopify_auth.decorators import login_required
 @login_required
 def index(request):
     # return render(request, 'index.html')
+    path = os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')
     try:
         print('before read index')
-        with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+        with open(path) as f:
             # return JsonResponse({'dfsdfsf': 'fdsfdsfdsf'}, safe=False)
             print('trying read index')
             return HttpResponse(f.read())
-    except FileNotFoundError:
+    except FileNotFoundError as e:
         logging.exception('Production build of app not found')
         return HttpResponse(
-            """
-            This URL is only used when you have built the production
-            version of the app. Visit http://localhost:3000/ instead, or
-            run `yarn run build` to test the production version.
-            """,
+            str(e) + '  ######' + path,
             status=501,
         )
