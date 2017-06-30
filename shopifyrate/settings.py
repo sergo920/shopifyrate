92692cba53c6e85eff5a69b023f935be85f53e2b
sergo <sergey@kilometer.io>
2017-06-30 23:39:15 +0300

    added react

--- shopifyrate/settings.py
+++ shopifyrate/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(REACT_APP_DIR, 'build')
+STATIC_ROOT = os.path.join(REACT_APP_DIR, 'build', 'static')
 SHOPIFY_APP_IS_EMBEDDED = True
 SHOPIFY_APP_DEV_MODE = False
 SHOPIFY_APP_NAME = 'Rate'
--- shopifyrate_app/views.py
+++ shopifyrate_app/views.py
@@ -1,24 +1,21 @@
 import os
 import logging
-from django.shortcuts import render
 from django.conf import settings
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from shopify_auth.decorators import login_required
 
 
 @login_required
 def index(request):
-    # return render(request, 'index.html')
     path = os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')
     try:
-        print('before read index')
         with open(path) as f:
-            # return JsonResponse({'dfsdfsf': 'fdsfdsfdsf'}, safe=False)
-            print('trying read index')
             return HttpResponse(f.read())
     except FileNotFoundError as e:
         logging.exception('Production build of app not found')
         return HttpResponse(
-            str(e) + '  ######' + settings.BASE_DIR + ' ###' + settings.REACT_APP_DIR,
+            [os.path.join(settings.ROOT_PATH, o) for o in os.listdir(settings.ROOT_PATH)
+             if os.path.isdir(os.path.join(settings.ROOT_PATH, o))],
+
             status=501,
         )
