2aa286a97f1298c61c258371a615bb31cec078bb
sergo <sergey@kilometer.io>
2017-07-01 00:05:15 +0300

    added react

--- shopifyrate/settings.py
+++ shopifyrate/settings.py
@@ -104,6 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
+# STATICFILES_DIRS = [
+#     os.path.join(REACT_APP_DIR, 'build', 'static'),
+# ]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
