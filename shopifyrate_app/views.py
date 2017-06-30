393d16337259a8d2d57c47fe72d52f133eaaf3b2
sergo <sergey@kilometer.io>
2017-06-30 23:44:59 +0300

    added react

--- shopifyrate_app/views.py
+++ shopifyrate_app/views.py
@@ -3,13 +3,14 @@ import logging
 from django.conf import settings
 from django.http import HttpResponse
 from shopify_auth.decorators import login_required
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
 @login_required
 def index(request):
     path = os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')
     try:
-        with open('/app/frontend/build/index.html') as f:
+        with open(static('index.html')) as f:
             return HttpResponse(f.read())
     except FileNotFoundError as e:
         logging.exception('Production build of app not found')
