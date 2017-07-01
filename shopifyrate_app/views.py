3d815f2aa78e05f209fdd9e43ceaab17bf1eaa2d
sergo <sergey@kilometer.io>
2017-07-01 09:50:23 +0300

    added react

--- shopifyrate_app/views.py
+++ shopifyrate_app/views.py
@@ -1,5 +1,4 @@
 import os
-from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 from shopify_auth.decorators import login_required
@@ -7,7 +6,5 @@ from shopify_auth.decorators import login_required
 
 @login_required
 def index(request):
-    path = os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')
-    return render(request, path)
     with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
         return HttpResponse(f.read())
