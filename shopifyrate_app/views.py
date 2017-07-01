a09618c9958784f15ff3368d0f385f6d4a223e5e
sergo <sergey@kilometer.io>
2017-07-01 09:25:22 +0300

    added react

--- shopifyrate_app/views.py
+++ shopifyrate_app/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 from shopify_auth.decorators import login_required
 from django.template.loader import get_template
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 @login_required
 def index(request):
+    return JsonResponse([os.path.join(settings.BASE_DIR,fn) for fn in next(os.walk(settings.BASE_DIR))[2]], safe=False)
+
     # path = staticfiles_storage.url('build/index.html')
     with open('/app/static/build/index.html') as f:
         return HttpResponse(f.read())
