37673a6ad67f095e8160533008e9020b5c8f0c37
sergo <sergey@kilometer.io>
2017-07-01 09:14:41 +0300

    added react

--- shopifyrate_app/views.py
+++ shopifyrate_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from shopify_auth.decorators import login_required
+from django.template.loader import get_template
 
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    return render(request, get_template('index.html'))
