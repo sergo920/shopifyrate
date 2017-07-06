9900d0611f47119a1140da5ce254a7deb0d73717
sergo <sergey@kilometer.io>
2017-07-07 00:25:18 +0300

    added facebook button

--- frontend/src/components/Root/index.js
+++ frontend/src/components/Root/index.js
@@ -7,7 +7,7 @@ import FacebookButton from "../FacebookButton";
 class Root extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="App container-fluid">
                 <FacebookButton/>
             </div>
         );
